3682011cd3b28ebf91d5e919b18f04a87641e2bf
arjun r <arjun.rdell@gmail.com>
2025-06-13 23:24:17 +0530

    calculating all parameter value for wellness score

--- src/Context/UserAuth.js
+++ src/Context/UserAuth.js
@@ -7,6 +7,7 @@ export const UserAuthProvider = ({ children }) => {
     const [userData, setUserData] = useState(null);
     const [serverUp, setServerUp] = useState(false);
     const [totalWellnessValue, setTotalWellnessValue] = useState("100%");
+    // const [wellnessValue, setWellnessValue] = useState({});
 
     useEffect(() => {
         Services.getUserAuth().then(res => {
@@ -18,18 +19,42 @@ export const UserAuthProvider = ({ children }) => {
     }, [])
 
     useEffect(() => {
-        let wellnessValue = 0;
-        userData?.data?.user?.parameters?.CRP.map((e) => {
-            wellnessValue = parseFloat(e.value) + wellnessValue
-        })
-        const negativeValuePerPercentage = (wellnessValue / userData?.data?.user?.parameters?.CRP.length) / 6
-        console.log(100 - (((wellnessValue / userData?.data?.user?.parameters?.CRP.length) - 6) * negativeValuePerPercentage))
-        setTotalWellnessValue(`${(100 - (((wellnessValue / userData?.data?.user?.parameters?.CRP.length) - 6) * negativeValuePerPercentage).toFixed(2))}%`)
+        let totalCRP = 0;
+        let totalESR = 0;
+
+        userData?.data?.user?.parameters?.CRP?.forEach((e) => {
+            totalCRP += e?.value ? parseFloat(e.value) : 0;
+        });
+
+        userData?.data?.user?.parameters?.ESR?.forEach((e) => {
+            totalESR += e?.value ? parseFloat(e.value) : 0;
+        });
+
+        // setWellnessValue((val) => ({
+        //     ...val,
+        //     CRP: totalCRP,
+        //     ESR: totalESR,
+        // }));
+
+        // userData?.data?.user?.parameters?.CRP.map((e) => {
+        //     if (e) setWellnessValue(val => { return { ...val, CRP: parseFloat(e.value) + wellnessValue?.CRP } });
+        //     else setWellnessValue(val => { return { ...val, CRP: 0 } });
+        // })
+        // userData?.data?.user?.parameters?.ESR.map((e) => {
+        //     if (e) setWellnessValue(val => { return { ...val, ESR: parseFloat(e.value) + wellnessValue?.ESR } });
+        //     else setWellnessValue(val => { return { ...val, ESR: 0 } });
+        // })
+        console.log(totalCRP)
+        const totalValue = (((totalCRP/userData?.data?.user?.parameters?.CRP.length)-6)*((totalCRP/userData?.data?.user?.parameters?.CRP.length)/6))+ 
+        (((totalESR/userData?.data?.user?.parameters?.ESR.length)-10)*((totalESR/userData?.data?.user?.parameters?.ESR.length)/10)) ;
+        // const negativeValuePerPercentage = (wellnessValue / userData?.data?.user?.parameters?.CRP.length) / 6
+        console.log(100 - totalValue)
+        setTotalWellnessValue(`${(100 - totalValue).toFixed(2)}%`)
         // console.log(userData)
     }, [userData])
 
     return (
-        <UserAuthContext.Provider value={{ userData, setUserData, serverUp,totalWellnessValue }}>
+        <UserAuthContext.Provider value={{ userData, setUserData, serverUp, totalWellnessValue }}>
             {children}
         </UserAuthContext.Provider>
     )
